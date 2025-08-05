2c909e354489ea6cc3742e2dfa5b7e8b83a2623a
ugy420 <ugyenwgyeltshen.ug@gmail.com>
2025-08-05 23:08:04 +0600

    header fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Home from "./Components/Home";
 import Header from "./Components/Header";
+import About from "./Components/About";
 
 export default function Page() {
   return (
     <div>
-      <Header/>
+      <header className={styles.header}>
+        <Header/>
+      </header>
       <Home/>
+      <About/>
       
     </div>
   );
