647ae43c82def5094069d351235d3dbaff57562b
ugy420 <ugyenwgyeltshen.ug@gmail.com>
2025-08-30 23:47:15 +0600

    motion

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,9 +1,27 @@
+"use client"
+
 import Button from "./ui/Button"
 import styles from './Header.module.css';
+import {motion, useScroll} from "motion/react"
 
 export default function Header(){
+    const { scrollYProgress } = useScroll()
+
     return (
+        
         <div className={styles.container}>
+            <motion.div 
+                style={{
+                    scaleX: scrollYProgress,
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    height: 5,
+                    originX: 0,
+                    backgroundColor: "#57C4E5",
+                }}
+            />
             <div>
                 <a href="">.ug</a>
             </div>
