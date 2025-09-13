28b446704081bf42a5d54e5f374f53fc48dc5a9d
unknown <ugyenwgyeltshen.ug@gmail.com>
2025-09-13 23:08:32 +0600

    strx

--- src/app/Components/About.tsx
+++ src/app/Components/About.tsx
@@ -8,10 +8,7 @@ export default function About() {
       <div className={styles.content}>
         <Image src={"/about.png"} alt="Me" width={250} height={200} />
         <div className={styles.text_div}>
-          Hello! I am Ugyen Wangda Gyeltshen. Lorem ipsum dolor sit amet
-          consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.
-          Pellentesque sit amet sapien fringilla, mattis ligula consectetur,
-          ultrices mauris. Maecenas vitae mattis tellus..
+          Hello! I am Ugyen Wangda Gyeltshen. I have a keen interest in web development and I want to be 
         </div>
       </div>
     </div>
