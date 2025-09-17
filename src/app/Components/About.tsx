658294aebac41ed7cba3cefe2279d9840b7b7b17
unknown <ugyenwgyeltshen.ug@gmail.com>
2025-09-17 14:05:35 +0600

    fin

--- src/app/Components/About.tsx
+++ src/app/Components/About.tsx
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 
 import styles from "./About.module.css";
 import Image from "next/image";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
     <motion.div
       initial={{ opacity: 0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{amount:0.5}}
+      viewport={{ amount: 0.5 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
       className={styles.container}
     >
@@ -22,8 +22,13 @@ export default function About() {
       <div className={styles.content}>
         <Image src={"/about.png"} alt="Me" width={250} height={200} />
         <div className={styles.text_div}>
-          Hello! I am Ugyen Wangda Gyeltshen. I have a keen interest in web
-          development and I want to be
+          <p>
+          Hello! I am Ugyen Wangda Gyeltshen, a developer from Bhutan, currently
+          working at DrukSmart. I dedicate myself to building impactful and
+          user-friendly applications. I enjoy exploring new technologies,
+          solving problems through code, and continuously learning to improve my
+          craft.
+          </p>
         </div>
       </div>
     </motion.div>
